959b08fba4af61154df47334c3b7293854350945
BobcatMK <matthew.kilan@gmail.com>
2015-02-28 18:38:01 +0100

    changes

---
+++ client/helpers/config.js
@@ -0,0 +1,7 @@
+// Accounts.ui.config({
+//     passwordSignupFields: "USERNAME_ONLY"
+// });
+
+Accounts.ui.config({   // This code allows us to login users via just username instead of email address
+    passwordSignupFields: "USERNAME_ONLY"
+})
