91248695d47d4f23cd8d532c934b176f07b13fe8
BobcatMK <matthew.kilan@gmail.com>
2015-03-02 22:35:14 +0100

    Added nextPath() to the controller

--- server/publications.js
+++ server/publications.js
@@ -5,14 +5,12 @@ Meteor.publish('posts', function(options) {
   });
   return Posts.find({}, options);
 });
+
 Meteor.publish('comments', function(postId) {
   check(postId, String);
-  return Comments.find({
-    postId: postId
-  });
+  return Comments.find({postId: postId});
 });
+
 Meteor.publish('notifications', function() {
-  return Notifications.find({
-    userId: this.userId
-  });
+  return Notifications.find({userId: this.userId});
 });
