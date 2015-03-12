fcfeb2a389e430d987a782d6cb3d6eccae0df74e
BobcatMK <matthew.kilan@gmail.com>
2015-03-12 21:13:38 +0100

    better upvoting algorithm

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -10,10 +10,18 @@ Template.postItem.helpers({
   // commentsCount: function() {
   //   return Comments.find({postId: this._id}).count();
   // }
+  upvotedClass: function() {
+    var userId = Meteor.userId();
+    if (userId && !_include(this.upvoters, userId)) {
+      return 'btn-primary upvotable';
+    } else {
+      return 'disabled';
+    }
+  }
 });
 
 Template.postItem.events({
-  'click .upvote': function(e) {
+  'click .upvotable': function(e) {
     e.preventDefault();
     Meteor.call('upvote',this._id);
   }
